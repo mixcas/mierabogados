075e3adde52659c6c30ada84932cfd4a6a0c1c11
cas <casska@gmail.com>
2015-10-30 18:06:42 -0600

    Init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* jshint browser: true, devel: true, indent: 2, curly: true, eqeqeq: true, futurehostile: true, latedef: true, undef: true, unused: true */
+/* global $, jQuery, document, Modernizr */
+
+jQuery(document).ready(function () {
+  'use strict';
+  l('Hola Globie');
+});
