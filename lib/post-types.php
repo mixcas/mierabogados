b87fdd65f05aef20858473d8cd3494e504102096
cas <casska@gmail.com>
2015-11-04 20:57:20 -0600

    add area post type

--- lib/post-types.php
+++ lib/post-types.php
@@ -17,6 +17,47 @@ add_action( 'admin_head', 'add_menu_icons_styles' );
 
 //Register Custom Post Types
 add_action( 'init', 'register_cpt_project' );
+add_action( 'init', 'register_cpt_areas' );
+function register_cpt_areas() {
+
+    $labels = array( 
+        'name' => _x( 'Areas', 'area' ),
+        'singular_name' => _x( 'Área', 'area' ),
+        'add_new' => _x( 'Agregar nueva', 'area' ),
+        'add_new_item' => _x( 'Agregar nueva Área', 'area' ),
+        'edit_item' => _x( 'Editar Área', 'area' ),
+        'new_item' => _x( 'Nueva Área', 'area' ),
+        'view_item' => _x( 'Ver Área', 'area' ),
+        'search_items' => _x( 'Buscar Áreas', 'area' ),
+        'not_found' => _x( 'No se encontraron areas', 'area' ),
+        'not_found_in_trash' => _x( 'No se encontraron aeas en la papelera', 'area' ),
+        'parent_item_colon' => _x( 'Parent Área:', 'area' ),
+        'menu_name' => _x( 'Áreas', 'area' ),
+    );
+
+    $args = array( 
+        'labels' => $labels,
+        'hierarchical' => false,
+        
+        'supports' => array( 'title', 'editor', 'thumbnail' ),
+        
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        
+        'show_in_nav_menus' => true,
+        'publicly_queryable' => true,
+        'exclude_from_search' => false,
+        'has_archive' => true,
+        'query_var' => true,
+        'can_export' => true,
+        'rewrite' => true,
+        'capability_type' => 'post'
+    );
+
+    register_post_type( 'area', $args );
+}
 
 function register_cpt_project() {
 
