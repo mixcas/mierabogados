a4605866b68df9907fbbc7a6a08cb9a082f740fa
cas <casska@gmail.com>
2015-11-22 22:37:40 -0600

    add single, wordpress styles and share bar

---
+++ partials/share.php
@@ -0,0 +1,5 @@
+<div class="share-bar quicksand">
+  <span>Compartir:</span>
+  <a href="#" class="facebook-share share-button">Facebook</a>
+  <a href="#" class="twitter-share share-button">Twitter</a>
+</div>
---
+++ single.php
@@ -0,0 +1,59 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content">
+
+  <!-- main posts loop -->
+  <section id="posts" class="container posts">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+?>
+
+    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+      <header>
+        <h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+        <p class="date"><?php the_date('d \/ m \/ Y'); ?>
+      </header>
+      
+      <?php the_post_thumbnail('single-featured', array( 'class' => 'single-featured' )); ?>
+
+      <?php get_template_part('partials/share'); ?>
+
+      <div class="post-content">
+      <?php the_content(); ?>
+        <hr />
+      </div>
+
+
+      <?php get_template_part('partials/share'); ?>
+
+    </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
+
+  <!-- end posts -->
+  </section>
+
+<hr />
+  <?php get_template_part('partials/contacto'); ?>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
